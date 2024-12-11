865a34d2e8085ea1bd2212966f82618a2ee62e09
wurafox <wurafoxxi@gmail.com>
2024-12-11 22:48:45 +0200

    Make code input square

--- include/DigitInput.h
+++ include/DigitInput.h
@@ -5,5 +5,7 @@
 class DigitInput : public QLineEdit {
 public:
     [[nodiscard]] QSize minimumSizeHint() const override;
+    [[nodiscard]] QSize sizeHint() const override;
+    [[nodiscard]] int heightForWidth(int w) const override;
     explicit DigitInput(QWidget* = nullptr);
 };
--- src/DigitInput.cpp
+++ src/DigitInput.cpp
@@ -6,9 +6,19 @@ DigitInput::DigitInput(QWidget* parent) : QLineEdit(parent) {
     QRegularExpression rx("\\d");
     auto* re = new QRegularExpressionValidator(rx, nullptr);
     this->setValidator(re);
-    setSizePolicy(QSizePolicy::Policy::Maximum, QSizePolicy::Policy::Expanding);
+    QSizePolicy sp(QSizePolicy::Preferred, QSizePolicy::Preferred);
+    sp.setHeightForWidth(true);
+    setSizePolicy(sp);
 }
 
 QSize DigitInput::minimumSizeHint() const {
-    return {20, 31};
+    return {20, 20};
+}
+
+int DigitInput::heightForWidth(int w) const {
+    return w;
+}
+
+QSize DigitInput::sizeHint() const {
+    return {48, 48};
 }
