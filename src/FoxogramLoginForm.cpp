fde50dee083732bb34e93acde7965408c2cf8ef4
wurafox <wurafoxxi@gmail.com>
2024-12-14 18:41:05 +0200

    Add validators to input fields, add password echo mode to password input field.

--- src/FoxogramLoginForm.cpp
+++ src/FoxogramLoginForm.cpp
@@ -3,12 +3,18 @@
 #include <IconButton.h>
 #include <iostream>
 #include <QMainWindow>
+#include <QRegularExpressionValidator>
 
 FoxogramLoginForm::FoxogramLoginForm(FoxogramMainWindow *parent) : QWidget(nullptr) {
     ui.setupUi(this);
     this->parent = parent;
     ui.line_2->setFixedHeight(1);
     ui.line_2->setStyleSheet("background-color: #606060");
+    QRegularExpression rxEmail(R"(^[_A-Za-z0-9-+]+(\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\.[A-Za-z0-9]+)*(\.[A-Za-z]{2,})$)");
+    ui.emailInput->setMaxLength(64);
+    ui.emailInput->setValidator(new QRegularExpressionValidator(rxEmail));
+    ui.passwordInput->setMaxLength(128);
+    ui.passwordInput->setEchoMode(QLineEdit::Password);
 }
 
 void FoxogramLoginForm::on_loginButton_clicked() {
--- src/FoxogramSignupForm.cpp
+++ src/FoxogramSignupForm.cpp
@@ -2,6 +2,7 @@
 #include <FoxogramMainWindow.h>
 #include <QDialog>
 #include <iostream>
+#include <QRegularExpressionValidator>
 
 FoxogramSignupForm::FoxogramSignupForm(FoxogramMainWindow* parent) : QWidget(nullptr) {
     ui.setupUi(this);
@@ -10,6 +11,14 @@ FoxogramSignupForm::FoxogramSignupForm(FoxogramMainWindow* parent) : QWidget(nul
     ui.line_2->setStyleSheet("background-color: #606060");
     modal = new FoxogramVerifyEmailModal(this);
     connect(parent, &FoxogramMainWindow::moved, modal, &FoxogramVerifyEmailModal::parentMoved);
+    QRegularExpression rxEmail(R"(^[_A-Za-z0-9-+]+(\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\.[A-Za-z0-9]+)*(\.[A-Za-z]{2,})$)");
+    ui.emailInput->setMaxLength(64);
+    ui.emailInput->setValidator(new QRegularExpressionValidator(rxEmail));
+    QRegularExpression rxUsername("^[_A-Za-z0-9-.]+$");
+    ui.usernameInput->setMaxLength(32);
+    ui.usernameInput->setValidator(new QRegularExpressionValidator(rxUsername));
+    ui.passwordInput->setMaxLength(128);
+    ui.passwordInput->setEchoMode(QLineEdit::Password);
 }
 
 void FoxogramSignupForm::on_registerButton_clicked() {
